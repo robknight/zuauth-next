b2989433ef3e318adb5309cb199efd967c6e2c52
Rob Knight <mail@robknight.org.uk>
2023-10-29 13:25:57 +0300

    Link to source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useZupass } from '@/zupass'
-import Image from 'next/image'
+import { useZupass } from "@/zupass";
 
 export default function Home() {
   const { login, ticketData } = useZupass();
@@ -9,12 +8,27 @@ export default function Home() {
       <div className="max-w-xl w-full">
         <h1 className="my-8 text-xl font-semibold">Login Example</h1>
         <div className="my-8">
-          <button className="rounded border-1 border-solid ring-1 ring-slate-500 border-slate-200 bg-slate-700 text-slate-100 hover:bg-slate-800 py-1 px-4" onClick={login}>Login</button>
+          <button
+            className="rounded border-1 border-solid ring-1 ring-slate-500 border-slate-200 bg-slate-700 text-slate-100 hover:bg-slate-800 py-1 px-4"
+            onClick={login}
+          >
+            Login
+          </button>
         </div>
-        {ticketData && <div className="my-8">
-          <div className="break-words">{JSON.stringify(ticketData)}</div>
-        </div>}
+        {ticketData && (
+          <div className="my-8">
+            <div className="break-words">{JSON.stringify(ticketData)}</div>
+          </div>
+        )}
+        <div className="mt-20">
+          <a
+            href="https://github.com/robknight/zuauth-next"
+            className="underline"
+          >
+            Source
+          </a>
+        </div>
       </div>
     </main>
-  )
+  );
 }
